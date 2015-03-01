c1fcb165a60027129e957f27563e9a6bc5a0e3bf
Aleksandr <rastomanchik77@gmail.com>
2015-03-01 20:55:38 +0200

    init

---
+++ BoardConfig.mk
@@ -0,0 +1,170 @@
+TARGET_SPECIFIC_HEADER_PATH := device/htc/primou/include
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/primou/overlay
+
+BOARD_VENDOR := htc
+
+#Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := primou
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_LOWMEM := true
+TARGET_ARCH_VARIANT_CPU := cortex-a8
+ARCH_ARM_HAVE_NEON := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+
+# Kernel
+KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/linaro-4.9-cortex-a8
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-cortex-a8
+TARGET_KERNEL_SOURCE := ~/android_kernel_ion
+TARGET_KERNEL_CONFIG := primou_defconfig
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x13F00000
+BOARD_KERNEL_PAGE_SIZE := 4096
+TARGET_KERNEL_NO_MODULES := true
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := legacy
+TARGET_PROVIDES_LIBLIGHT := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Audio
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
+BOARD_HAVE_HTC_AUDIO := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+
+# Display
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_NO_HW_VSYNC := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_EGL_CFG := device/htc/primou/configs/egl.cfg
+TARGET_USES_ION := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK 
+
+# Camera
+BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
+CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+BOARD_USES_PMEM_ADSP := true
+COMMON_GLOBAL_CFLAGS += -DNO_UPDATE_PREVIEW -DICS_CAMERA_BLOB -DQCOM_LEGACY_CAM_PARAMS
+COMMON_GLOBAL_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/primou/bluetooth/vnd_msm7x30.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/primou/bluetooth/include
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := primou
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# RIL
+BOARD_USE_NEW_LIBRIL_HTC := true
+BOARD_USES_LEGACY_RIL := true
+
+# Usb
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+# WIFI
+WIFI_BAND := 802_11_ABGN
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+BOARD_LEGACY_NL80211_STA_EVENTS := true
+
+# Parttions and filesystem
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+
+# TWRP
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_INITRC := device/htc/primou/recovery/root/init.rc
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_MTP := true
+TW_FLASH_FROM_STORAGE := true
+TW_NO_CPU_TEMP := true
+TW_NO_SCREEN_BLANK := true
+TW_USE_TOOLBOX := true
+
+# We are not want build recovery
+TARGET_RECOVERY_FSTAB := device/htc/primou/ramdisk/fstab.primou
+
+# MISC
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+BOARD_SKIP_ANDROID_DOC_BUILD := true
+# No SDK blobs
+BUILD_EMULATOR := false
+BUILD_EMULATOR_SENSORS_MODULE := false
+BUILD_EMULATOR_GPS_MODULE := false
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/htc/primou/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    file_contexts \
+    hci_init.te \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    untrusted_app.te \
+    vold.te \
+    wpa.te \
+    wpa_socket.te
---
+++ primou.mk
@@ -0,0 +1,168 @@
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+# Common proprietary blobs
+$(call inherit-product-if-exists, vendor/htc/msm7x30-common/msm7x30-vendor.mk)
+
+# Vendor
+$(call inherit-product, vendor/htc/primou/primou-vendor.mk)
+
+# TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/sbin/choice_fn:recovery/root/sbin/choice_fn \
+    $(LOCAL_PATH)/recovery/root/sbin/detect_key:recovery/root/sbin/detect_key \
+    $(LOCAL_PATH)/recovery/root/sbin/htcbatt:recovery/root/sbin/htcbatt \
+    $(LOCAL_PATH)/recovery/root/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
+    $(LOCAL_PATH)/recovery/root/sbin/power_test:recovery/root/sbin/power_test \
+    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Media Profiles
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+# Common GPS config
+PRODUCT_COPY_FILES += \
+	device/common/gps/gps.conf_EU_SUPL:system/etc/gps.conf
+
+# Device-Specific HALs
+PRODUCT_PACKAGES += \
+	lights.msm7x30 \
+	sensors.msm7x30 \
+	power.msm7x30 \
+	gps.primou
+
+PRODUCT_PACKAGES += \
+	libbt-vendor \
+	librpc
+
+# Filesystem
+PRODUCT_PACKAGES += \
+	make_ext4fs \
+	setup_fs \
+	e2fsck
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.primary.msm7x30 \
+	audio_policy.msm7x30 \
+	libaudioutils
+	
+# Video
+PRODUCT_PACKAGES += \
+	copybit.msm7x30 \
+	gralloc.msm7x30 \
+	hwcomposer.msm7x30
+
+# QCOM OMX
+PRODUCT_PACKAGES += \
+	libOmxCore \
+	libOmxVdec \
+	libOmxVenc \
+	libc2dcolorconvert \
+	libstagefrighthw
+	
+PRODUCT_PACKAGES += \
+	Torch \
+	com.android.future.usb.accessory \
+	libsurfaceflinger_client \
+	libnetcmdiface
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/init.primou.rc:root/init.primou.rc \
+	$(LOCAL_PATH)/ramdisk/init.htc7x30.usb.rc:root/init.htc7x30.usb.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.primou.rc:root/ueventd.primou.rc \
+	$(LOCAL_PATH)/ramdisk/fstab.primou:root/fstab.primou
+
+# Input device config
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/idc/primou-keypad.idc:system/usr/idc/primou-keypad.idc \
+	$(LOCAL_PATH)/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+
+# Keylayouts and keychars
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+	$(LOCAL_PATH)/keylayout/primou-keypad.kl:system/usr/keylayout/primou-keypad.kl \
+	$(LOCAL_PATH)/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl
+
+# ACDB
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/default.acdb:system/etc/firmware/default.acdb \
+	$(LOCAL_PATH)/configs/default_org.acdb:system/etc/firmware/default_org.acdb
+
+# Audio DSP Profiles
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    $(LOCAL_PATH)/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+	$(LOCAL_PATH)/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
+	$(LOCAL_PATH)/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv 
+
+# Apps
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/apps/FileManager.apk:data/app/FileManager.apk \
+	$(LOCAL_PATH)/apps/MX_Player.apk:data/app/MX_Player.apk \
+	$(LOCAL_PATH)/apps/FMRadio.apk:data/app/FMRadio.apk
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+# Misc. property overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.setupwizard.enable_bypass=1 \
+	ro.com.google.locationfeatures=1 \
+	ro.com.google.networklocation=1 \
+	ro.com.google.gmsversion=4.0_r1 \
+	dalvik.vm.lockprof.threshold=500 \
+	dalvik.vm.dexopt-flags=m=y \
+	ro.zygote.disable_gl_preload=true \
+	ro.config.low_ram=true \
+	ro.sf.lcd_density=240 \
+	ro.vold.umsdirtyratio=20 \
+	ro.sys.fw.bg_apps_limit=12 \
+	ro.config.max_starting_bg=6 \
+	debug.sf.hw=1 \
+	debug.egl.hw=1 \
+	debug.egl.recordable.rgba8888=1 \
+	debug.sf.no_hw_vsync=1 \
+	debug.mdpcomp.logs=0 \
+	dev.pm.dyn_samplingrate=1 \
+	ro.opengles.version=131072 \
+	ro.telephony.ril.v3=signalstrength \
+	com.qc.hardware=true \
+	debug.composition.type=gpu \
+	persist.sys.usb.config=mass_storage,adb \
+	media.a1026.nsForVoiceRec=0 \
+	media.a1026.enableA1026=0 \
+	htc.audio.alt.enable=0 \
+	htc.audio.hac.enable=0
+
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+
+# use high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
